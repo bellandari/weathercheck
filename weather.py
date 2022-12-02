84fa036d93bdadf58a3aad1f635e991697e409b5
bellandari <bellandari@feartheaquapickle.com>
2022-12-02 11:28:21 -0700

    Reformat, added header

--- weather.py
+++ weather.py
@@ -1,36 +1,26 @@
+# Code by: Bellandari
+# GitHub: https://github.com/bellandari
+# Last Update: 12/02/2022
+
 import requests
-import os
-import sys
 import key
 import click
-
-#os.system('cls || clear')
-
-#class weathercheck:
         
 @click.command()
 @click.option("--city", "-c", prompt = True)
 
 def weather(city):
     
-    # Pulls API Key from hidden file
     password = key.keycode
-    
-    # Asks user for name of city 
-    
-    # Response from API
     response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={city}&units=imperial&appid={password}")
     
-    # Handles error if typo in city
     if response.json()['cod'] == "404":
         
         click.echo ("Was there a typo? Try again!")
-        
         weather()
         
     else: 
-        
-        # Dictionary for responses
+    
         temp = round(response.json()['list'][0]['main']['temp'])
         feelslike = round(response.json()['list'][0]['main']['feels_like'])
         humidity = response.json()['list'][0]['main']['humidity']
